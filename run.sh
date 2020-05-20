d1a8bc04a91c797f58fb0258f9c743cbaa788fdc
David Martinez <divad4686@gmail.com>
2020-05-20 22:36:01 +0200

    add go project

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+cd src
+go run main.go
---
+++ src/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"golang/cicd/todo/morestrings"
+)
+
+func main() {
+	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+}

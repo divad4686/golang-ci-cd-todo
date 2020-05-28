7cc77df81568e66b9f53c3229e892de5aad00ddd
David Martinez <divad4686@gmail.com>
2020-05-28 16:33:21 +0200

    add tests

--- todoapp/main.go
+++ todoapp/main.go
@@ -2,11 +2,21 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+func Sum(a, b int) int {
+	return a + b
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
+		})
+	})
+
+	r.GET("/sum", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": Sum(2, 2),
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
